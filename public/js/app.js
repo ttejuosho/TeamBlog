6af2123c62bcf40d16fb29a0fc2aab5e0782a820
Taiwo <theycallmeflowz@yahoo.com>
2018-02-26 23:17:22 -0600

    js form

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,25 @@ $(document).ready(function(){
 
     $(".button-collapse").sideNav();
 
+    var firstName = $("#firstName");
+    var lastName = $("#lastName");
+    var email = $("#email");
+    var location = $("#location");
+    var role = $("#role");
+    var bio = $("#bio");
 
+    $(signup).on("submit", function createUser(event){
+
+        var newUser = {
+            First_Name: firstName.val().trim(),
+            Last_Name: lastName.val().trim(),
+            Email: email.val().trim(),
+            Location: location.val().trim(),
+            Role: role.val().trim(),
+            Bio: bio.val().trim()
+        };
+
+        console.log(newUser);
+    })
 
 });
