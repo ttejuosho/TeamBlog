02cf8b549237f3ef92548a468f1c2162fc02fa63
Taiwo <theycallmeflowz@yahoo.com>
2018-02-28 18:44:16 -0600

    js app

--- routes/userInfo-routes.js
+++ routes/userInfo-routes.js
@@ -2,8 +2,8 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-    // Get User Info from API
-    app.get('/api/userinfo/:id', function(req, res){
+    // Get Users Info from DB
+    app.get('/api/userinfo', function(req, res){
         db.UserInfo.findAll({
             where: {
                 id: req.params.id
@@ -13,15 +13,27 @@ module.exports = function(app) {
         });
     });
 
-    // Post User Info to API
-    app.post('/api/userinfo/', function(req, res){
-        db.UserInfo.create({
-            UserInfo: 'TBD',
-            UserId: req.params.id
-        }).then(function(dbUserInfo){
-            res.json(dbUserInfo);
-        });
-    });
+    app.get('/api/userinfo/:id', function(req,res){
+        // Find One User by ID and return them in Json format 
+            db.UserInfo.findOne({
+                where: {
+                    id: req.params.id
+                }
+            }).then(function(dbUserInfo){
+                res.json(dbUserInfo);
+            });
+        });//end of getId function
+
+
+        // Post User Info to API
+    app.post('/api/userinfo', function(req, res){
+        // Creates a User with available data
+            console.log(req.body);
+            db.UserInfo.create(req.body).then(function(dbUserInfo) {
+                res.json(dbUserInfo);
+            });
+        }); //end of post route
+
 
        // Update User Info 
    app.put('/api/userinfo/:id', function(req, res){
